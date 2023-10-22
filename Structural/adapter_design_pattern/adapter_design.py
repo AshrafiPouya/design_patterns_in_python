345d2423e4f76a8f4a422ae148fd90279ba6aa66
pouyaashrafidev <8385876839>
2023-10-22 14:19:48 +0330

    add adapater design pattern

---
+++ Structural/adapter_design_pattern/adapter_design.py
@@ -0,0 +1,35 @@
+class Product:
+    def __init__(self, name, type, price) -> None:
+        self.type = type
+        self.price = price
+        self.name = name
+
+
+class WageAdapter:
+    def __init__(self) -> None:
+        self.products = {}
+        pass
+
+
+    def calculate_wage(self, product):
+        return (self.products[product.type] + 1) * product.price
+    
+
+    def add(self, type, percent):
+        self.products[type] = percent
+
+
+
+if __name__ == "__main__":
+    product1 = Product("a", "gold", price=120)
+    product2 = Product("b", "silver", price=80)
+    product3 = Product("c", "other", price=20)
+
+    adapter = WageAdapter()
+    adapter.add("gold", 0.2)
+    adapter.add("silver", 0.1)
+    adapter.add("other", 0.5)
+
+    print(adapter.calculate_wage(product1))
+    print(adapter.calculate_wage(product2))
+    print(adapter.calculate_wage(product3))
